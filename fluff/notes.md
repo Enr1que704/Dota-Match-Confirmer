54b2c3801b6c6b6f54fe2a5ff476352719b295bd
Enrique <ericdavidjohns@gmail.com>
2023-10-23 15:46:12 -0600

    setting up project structure

---
+++ fluff/notes.md
@@ -0,0 +1,2 @@
+https://stackoverflow.com/questions/65171433/run-a-python-code-only-when-a-specific-object-appears-in-screen
+PyAutoGui
---
+++ src/main.py
@@ -0,0 +1,9 @@
+import pyautogui as pgui
+pgui.PAUSE = 0.2
+
+size = pgui.size()
+print(size)
+
+location = pgui.position()
+print(location)
+
