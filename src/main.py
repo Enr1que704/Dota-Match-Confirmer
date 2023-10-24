828896b12bec2fd9d5c65ca5675a7e28bf205335
Enrique <ericdavidjohns@gmail.com>
2023-10-23 18:30:04 -0600

    more structure

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import pyautogui as pgui
+import sys
 pgui.PAUSE = 0.2
 
-size = pgui.size()
-print(size)
+location = pgui.locateOnScreen('..\\images\\accept.png')
+center = pgui.center(location)
+pgui.moveTo(center)
 
-location = pgui.position()
-print(location)
+# print('Press Ctrl-C to quit.')
+# try:
+#     while True:
+#         x, y = pgui.position()
+#         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+#         print(positionStr, end='')
+#         print('\b' * len(positionStr), end='', flush=True)
+# except KeyboardInterrupt:
+#     print('\n')
 
