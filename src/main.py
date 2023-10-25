6486feecac816a7400ba4eefd30cc474a6ca0c56
Eric Johnson <ericdavidjohns@gmail.com>
2023-10-24 22:09:05 -0600

    added telegram message support

--- src/main.py
+++ src/main.py
@@ -1,40 +1,36 @@
 import pyautogui as pgui
-import sys, time
-import requests
+import sys, time, smtplib, requests
+from email.message import EmailMessage
+
+TOKEN = '6442084061:AAHszPjHDUr3Ssvm5gs2Nrz9-zQU9E46WEQ'  # bot token
+chat_id = 6576780272
 pgui.PAUSE = 0.2
 
-EMAIL = "enriquescodeproject@gmail.com"
-PASSWORD = "codeproject"
+
+# url = f"https://api.telegram.org/bot{TOKEN}/getUpdates" ## use this to get the chat id
+# print(requests.get(url).json())
 
 
-# go = True
-# while go:
-#     time.sleep(5)
-#     try:
-#         location = pgui.locateOnScreen('..\\images\\moonshard_accept.png', confidence=0.7)
-#         center = pgui.center(location)
-#         pgui.moveTo(center)
-#         pgui.leftClick()
-#         go = False
-#     except:
-#         print("Accept button not found, trying again")
-#
+def send_alert(message):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    requests.get(url)
+
+
+go = True
+while go:
+    time.sleep(5)
+    try:
+        location = pgui.locateOnScreen('..\\images\\moonshard_accept.png', confidence=0.7)
+        center = pgui.center(location)
+        pgui.moveTo(center)
+        pgui.leftClick()
+        send_alert("Your game is ready")
+        go = False
+    except:
+        print("Accept button not found, trying again")
 
 
 
 
 
 
-
-# send discord notifications (too bad it doesn't play nice with being idle and sending them on your phone
-
-# payload = {
-#     'content': "Your game is ready"
-# }
-#
-# header = {
-#     'authorization': 'MTE2NjI1MjU4NzIyNjQ5NzExNA.Gzsf0d.M-StBu1hIbdvdMOXDyogKA7BzHg1iXqiMTwyfU', ## get this from the network tab header
-#     'content-type': 'application/json'
-# }
-#
-# r = requests.post("https://discord.com/api/v9/channels/1166250272499187712/messages", json=payload, headers=header)
