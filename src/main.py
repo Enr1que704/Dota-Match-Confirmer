46ab75f6a2a79373a98e0a58f21c6242d64e3e4b
Eric Johnson <ericdavidjohns@gmail.com>
2023-10-25 00:14:59 -0600

    cleaned up code, updated status messages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+import config
+import requests
+import time
+
 import pyautogui as pgui
-import sys, time, requests, config
 
 pgui.PAUSE = 0.2
 
@@ -9,23 +12,27 @@ pgui.PAUSE = 0.2
 
 
 def send_alert(message):
-    url = f"https://api.telegram.org/bot{config.SETTINGS['token']}/sendMessage?chat_id={config.SETTINGS['chatId']}&text={message}"
+    url = f"https://api.telegram.org/bot{config.SETTINGS['token']}" \
+          f"/sendMessage?chat_id={config.SETTINGS['chatId']}&text={message}"
     requests.get(url)
 
 
-go = True
-while go:
-    time.sleep(5)
-    try:
-        location = pgui.locateOnScreen('..\\images\\moonshard_accept.png', confidence=0.7)
-        center = pgui.center(location)
-        pgui.moveTo(center)
-        pgui.leftClick()
-        send_alert("Your game is ready")
-        go = False
-    except:
-        print("Accept button not found, trying again")
+def run():
+    go = True
+    while go:
+        time.sleep(5)
+        try:
+            location = pgui.locateOnScreen('..\\images\\moonshard_accept.png', confidence=0.7)
+            center = pgui.center(location)
+            pgui.moveTo(center)
+            pgui.leftClick()
+            send_alert("Your game is ready")
+            print("Game found!")
+            go = False
+        except:
+            print("Looking for game...")
 
 
-
+if __name__ == '__main__':
+    run()
 
