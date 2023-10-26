a7fcb338d8a3e599bb6e221420baa918d3d29657
Eric Johnson <ericdavidjohns@gmail.com>
2023-10-26 12:21:31 -0600

    added get_main_attr method to find the inputted heroes main attr

--- src/heroOrganizer.py
+++ src/heroOrganizer.py
@@ -2,9 +2,33 @@ import HEROES as h
 
 
 def get_location(hero):
-    position = h.STRENGTH.index(hero)
+    attribute = get_main_attr(hero)
+
+    match attribute:
+        case "STRENGTH":
+            position = h.STRENGTH.index(hero)
+        case "AGILITY":
+            position = h.AGILITY.index(hero)
+        case "INTELLIGENCE":
+            position = h.INTELLIGENCE.index(hero)
+        case "UNIVERSAL":
+            position = h.UNIVERSAL.index(hero)
+        case _:
+            position = -1
+
     column = position % 11
     row = position // 11
     print(str(column) + " " + str(row))
 
 
+def get_main_attr(hero):
+    if hero in h.STRENGTH:
+        return "STRENGTH"
+    elif hero in h.AGILITY:
+        return "AGILITY"
+    elif hero in h.INTELLIGENCE:
+        return "INTELLIGENCE"
+    elif hero in h.UNIVERSAL:
+        return "UNIVERSAL"
+    else:
+        return "Not found"
